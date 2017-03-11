cab8cc06a602815ce0633500c02b49adf5646f43
place222 <place222@hotmail.com>
2017-03-11 12:20:36 +0800

    升级2017

--- src/MyFirstCoreWeb/wwwroot/_references.js
+++ src/MyFirstCoreWeb/wwwroot/_references.js
@@ -4,6 +4,7 @@
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/icheck/icheck.min.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-pjax/jquery.pjax.js" />
 /// <reference path="lib/jquery-ui/jquery-ui.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
