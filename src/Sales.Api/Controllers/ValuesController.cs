5d933318b9ff9fd5b939279c936cffc176e6b591
place222 <place222@hotmail.com>
2017-03-12 23:07:42 +0800

    添加销售模块

---
+++ src/Sales.Api/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sales.Api.Models;
+
+namespace Sales.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private AdventureWorks2012Context _db;
+
+        public ValuesController(AdventureWorks2012Context db)
+        {
+            _db = db;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return _db.Product.FirstOrDefault(x => x.ProductId == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
